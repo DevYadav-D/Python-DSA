45b05fef252d58ba6042512f7194e7496986678b
DevYadav-D <devyadav5392@gmail.com>
2023-09-13 12:46:31 +0200

    Given an array A of size N, your task is to do some operations, i.e., search an element x, insert an element y at index yi, and delete an element z by completing the functions. Also, all functions should return a boolean value.

---
+++ operatingAnArray.py
@@ -0,0 +1,29 @@
+# Your task is to complete all
+# three functions
+
+# if the element is found in the list
+# function  must return true or else
+# return false
+def searchEle(a, x):
+    # Code here
+    if x in a:
+        return True
+    else:
+        return False
+
+# fucntion must return true if 
+# insertion is successful or else
+# return false
+def insertEle(a, y, yi):
+    # Code here
+    a[yi] = y
+    return searchEle(a,y)
+
+# fucntion must return true if 
+# deletion is successful or else
+# return false
+def deleteEle(a, z):
+    # Code here
+    if z in a:
+        a.remove(z)
+    return True
